1d2e4d5b9e12aa4bac9c46355d55352c3dbc4434
sparkbold-git <sparkboldstudio@gmail.com>
2018-10-30 08:43:06 -0500

    done but the test not running

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React, { Component } from "react";
+
+export default class EyesOnMe extends Component {
+  handleFocus = () => {
+    console.log("Hey! Eyes on me!");
+  };
+
+  handleBlur = () => {
+    console.log("Good!");
+  };
+  render() {
+    return (
+      <div>
+        <button onFocus={this.handleFocus()} onBlur={this.handleBlur()}>
+          Hello
+        </button>
+      </div>
+    );
+  }
+}
